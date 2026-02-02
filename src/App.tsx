70cb3dc2295f51ce0c880361ad52b3e65f13f199
Ghaida Ja'aysah <ghaida.20004@gmail.com>
2026-02-02 21:47:25 +0200

    feat: Design Upcoming Session Page with static Data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-import RequestSkill from './pages/RequestSkill/RequestSkill';
-import './index.css';
+import "./index.css";
+import { RequestSkill } from "./pages/RequestSkill";
+import { SessionSchedule } from "./pages/session/SessionSchedule";
+import type { Session } from "./pages/session/SessionSchedule";
+// import { UpcomingSessionCard } from "./pages/session/UpcomingSessionCard";
 
+const mockSessions: Session[] = [
+  {
+    id: "1",
+    category: "Spanish Conversation",
+    title: "Basic of Spanish",
+    date: "Saturday, Dec 16",
+    time: "3:00 PM - 4:00 PM",
+    partnerName: "Jane Cooper",
+  },
+  {
+    id: "2",
+    category: "English Speaking",
+    title: "Business English",
+    date: "Monday, Dec 18",
+    time: "6:00 PM - 7:00 PM",
+    partnerName: "Robert Fox",
+  },
+];
 function App() {
   return (
-    <RequestSkill />
+    <>
+      {/* <SessionSchedule sessions={mockSessions} /> */}
+      <RequestSkill />
+    </>
   );
 }
 
---
+++ src/routes/Routers.js
@@ -0,0 +1,13 @@
+// import React from "react";
+// import { Routes, Route } from "react-router-dom";
+
+// function Routers() {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<RequestSkill />} />
+//       <Route path="/session" element={<UpcomingSessionCard />} />
+//     </Routes>
+//   );
+// }
+
+// export default Routers;
